d07016eeb37d002f8b17ada752af4d7d844b0470
irina.patularu <irinapatularu@yahoo.com>
2016-06-22 17:17:30 +0300

    Added json parser and view also for zip and country name

--- grails-app/controllers/weatherapp/WeatherController.groovy
+++ grails-app/controllers/weatherapp/WeatherController.groovy
@@ -9,16 +9,14 @@ class WeatherController {
 
     def save() {
         def input = params.location
-        int code = (int)input[0]
-        if (code <= 9 && code >= 0){
-            def response = weatherFetchService.getDataByCity(params.location)
+        def response
+        if (!input.matches(".*\\d.*")) {
+            response = weatherFetchService.getDataByCity(params.location)
         }
         else{
-            def response = weatherFetchService.getDataByZip(params.location)
+            response = weatherFetchService.getDataByZip(params.location)
         }
 
         render(view: 'show', model: ['location':response.location, 'temp':response.temp, 'humidity':response.humidity, 'country':response.country, 'description':response.description])
-
-
     }
 }
