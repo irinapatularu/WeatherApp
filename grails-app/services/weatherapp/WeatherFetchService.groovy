2c4f1572f91e7c89e8236a5c8bd384158b6f10ca
irina.patularu <irinapatularu@yahoo.com>
2016-06-22 15:34:21 +0300

    Added json parser and view

--- grails-app/services/weatherapp/WeatherFetchService.groovy
+++ grails-app/services/weatherapp/WeatherFetchService.groovy
@@ -18,13 +18,13 @@ class WeatherFetchService {
         myUrl = myUrl + appid
 
         String mock = '{"coord":{"lon":-0.13,"lat":51.51},"weather":[{"id":500,"main":"Rain","description":"light rain","icon":"10d"}],"base":"cmc stations","main":{"temp":292.44,"pressure":1010,"humidity":92,"temp_min":290.37,"temp_max":294.82},"wind":{"speed":0.51,"deg":135,"gust":2.06},"rain":{"1h":0.25},"clouds":{"all":100},"dt":1466592619,"sys":{"type":3,"id":54708,"message":0.004,"country":"GB","sunrise":1466567013,"sunset":1466626909},"id":2643743,"name":"London","cod":200}'
-
         JSONElement userJson = JSON.parse(mock)
 
         String sys = "sys"
         String weather = "weather"
         String main = "main"
         String name = "name"
+
         String temp = userJson[main].temp;
         String humidity = userJson[main].humidity;
         String country = userJson[sys].country;
@@ -38,7 +38,6 @@ class WeatherFetchService {
         values['country'] = country
         values['location'] = loc
         values['description'] = description
-        //values = values + temp + humidity + country + loc + description
         return values
     }
 }
